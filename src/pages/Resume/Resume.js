6b77c6a0986eb828f2dbfac52486ac403b1cf132
Giancarlo Junior <junior.giancarlo@gmail.com>
2021-01-14 13:19:12 -0300

    new changes

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -1,63 +1,145 @@
-import { Grid, Typography } from '@material-ui/core';
-import React from 'react';
-import resumeData from '../../utils/resumeData';
-import CustomTimeline, { CustomTimelineSeparator } from "../../components/Timeline/Timeline";
-import WorkIcon from '@material-ui/icons/Work';
+import { Grid, Icon, Paper, Typography } from "@material-ui/core";
+import React from "react";
+import resumeData from "../../utils/resumeData";
+import CustomTimeline, {
+  CustomTimelineSeparator,
+} from "../../components/Timeline/Timeline";
+import { Work as WorkIcon, School as SchoolIcon } from "@material-ui/icons";
 
-import './Resume.css';
+import "./Resume.css";
+import TimelineDot from "@material-ui/lab/TimelineDot";
+// import TimelineConnector from "@material-ui/lab/TimelineConnector";
+import TimelineItem from "@material-ui/lab/TimelineItem";
+import TimelineContent from "@material-ui/lab/TimelineContent";
 
 const Resume = () => {
-    return (
-        <>
-            
-            {/* about me */}
-            <Grid container className='section'>
-                <Grid item className='section_title mb_30'>
-                    <span></span>
-                    <h6 className='section_title_text'>About Me</h6>
-                </Grid>
-                <Grid item xs={12}>
-                    <Typography variant='body2' className='aboutme_text'>{resumeData.about}</Typography>
-                </Grid>
-            </Grid>
+  return (
+    <>
+      {/* about me */}
+      <Grid container className="section">
+        <Grid item className="section_title mb_30 pt_45">
+          <span></span>
+          <h6 className="section_title_text">About Me</h6>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="body2" className="aboutme_text">
+            {resumeData.about}
+          </Typography>
+        </Grid>
+      </Grid>
 
+      <Grid container className="section">
+        <Grid item className="section_title mb_30">
+          <span></span>
+          <h6 className="section_title_text">Resume</h6>
+        </Grid>
+
+        {/* esperiences & education */}
+        <Grid item xs={12}>
+          <Grid container className="resume_timeline">
             {/* experiences */}
-            <Grid container className='section'>
-                <Grid item className='section_title mb_30'>
-                    <span></span>
-                    <h6 className='section_title_text'>Resume</h6>
-                </Grid>
-            <Grid item xs={12}>
-                <Grid container>
-                    {/* experiences */}
-                    <Grid item sm={12} md={6}>
-                        <CustomTimeline title='Work Experience' icon={<WorkIcon />}>
-                            {resumeData.experiences.map(experience => (
-                                
-                            ))}
-
-                        </CustomTimeline>
-                    </Grid>
-                    {/* education */}
-                    <Grid item sm={12} md={6}>
-
-                    </Grid>
-                </Grid>
-            </Grid>
+            <Grid item sm={12} md={6}>
+              <CustomTimeline title="Work Experience" icon={<WorkIcon />}>
+                {resumeData.experiences.map((experience) => (
+                  <TimelineItem>
+                    <CustomTimelineSeparator />
+                    <TimelineContent>
+                      <Typography className="timeline_title">
+                        {experience.title}
+                      </Typography>
+                      <Typography variant="caption" className="timeline_date">
+                        {experience.date}
+                      </Typography>
+                      <Typography variant="body2" className="timeline_desc">
+                        {experience.desc}
+                      </Typography>
+                    </TimelineContent>
+                  </TimelineItem>
+                ))}
+              </CustomTimeline>
             </Grid>
 
-            {/* services */}
-            <Grid container className='section'></Grid>
+            {/* education */}
+            <Grid item sm={12} md={6}>
+              <CustomTimeline title="Education" icon={<SchoolIcon />}>
+                {resumeData.education.map((education) => (
+                  <TimelineItem>
+                    <CustomTimelineSeparator />
+                    <TimelineContent>
+                      <Typography className="timeline_title">
+                        {education.title}
+                      </Typography>
+                      <Typography variant="caption" className="timeline_date">
+                        {education.date}
+                      </Typography>
+                      <Typography variant="body2" className="timeline_desc">
+                        {education.desc}
+                      </Typography>
+                    </TimelineContent>
+                  </TimelineItem>
+                ))}
+              </CustomTimeline>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
 
-            {/* skills */}
-            <Grid container className='section'></Grid>
+      {/* services */}
+      <Grid container className="section pb_45">
+        <Grid item className="section_title mb_30">
+          <span></span>
+          <h6 className="section_title_text">Services</h6>
+        </Grid>
 
-            {/* contact */}
-            <Grid container className='section'></Grid>
+        <Grid item xs={12}>
+          <Grid container spacing={3} justify="space-around">
+            {resumeData.services.map((service) => (
+              <Grid item xs={12} sm={6} md={3}>
+                <div className="service">
+                  <Icon className="service_icon">{service.icon}</Icon>
+                  <Typography className="service_title" variant="h6">
+                    {service.title}
+                  </Typography>
+                  <Typography className="service_desc" variant="body2">
+                    {service.desc}
+                  </Typography>
+                </div>
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
 
-        </>
-    )
+      {/* skills */}
+      <Grid container className="section graybg pb_45 p_50">
+        <Grid item xs={12}>
+          <Grid container justify="space-between" spacing={3}>
+            {resumeData.skills.map((skill) => (
+              <Grid item xs={12} sm={6} md={3}>
+                <Paper elevation={0} className="skill">
+                  <Typography variant="h6" className="skills_title">
+                    {skill.title}
+                  </Typography>
+                  {skill.desc.map((element) => (
+                    <Typography variant="body2" className="skill_desc">
+                      <TimelineDot
+                        variant={"outlined"}
+                        className="timeline_dot"
+                      />
+                      {element}
+                    </Typography>
+                  ))}
+                </Paper>
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
 
-}
+      {/* contact */}
+      <Grid container className="section"></Grid>
+    </>
+  );
+};
 
-export default Resume
+export default Resume;
--- src/utils/resumeData.js
+++ src/utils/resumeData.js
@@ -2,6 +2,7 @@ import React from "react";
 import TwitterIcon from '@material-ui/icons/Twitter';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import { AssignmentOutlined, WebOutlined } from "@material-ui/icons";
 
 
 // eslint-disable-next-line import/no-anonymous-default-export
@@ -49,5 +50,78 @@ export default {
                 date: '2018 - Present',
                 desc: 'Expenses as material breeding insisted building to in. Continual so distrusts pronounce by unwilling listening. Thing do taste on we manor.',
             },
-        ]
-}
+        ],
+        education: [
+            {
+                title: 'edu',
+                date: '2018 - Present',
+                desc: 'Expenses as material breeding insisted building to in. Continual so distrusts pronounce by unwilling listening. Thing do taste on we manor.',
+            },
+            {
+                title: 'edu 2',
+                date: '2018 - Present',
+                desc: 'Expenses as material breeding insisted building to in. Continual so distrusts pronounce by unwilling listening. Thing do taste on we manor.',
+            },
+            {
+                title: 'edu 3',
+                date: '2018 - Present',
+                desc: 'Expenses as material breeding insisted building to in. Continual so distrusts pronounce by unwilling listening. Thing do taste on we manor.',
+            },
+        ],
+
+        services: [
+            {
+                title: 'web dev',
+                desc: 'Tempor dolor ad irure est Lorem magna ea culpa minim anim aliqua.',
+                icon: <WebOutlined />
+            },
+            {
+                title: 'brand identity',
+                desc: 'Tempor dolor ad irure est Lorem magna ea culpa minim anim aliqua.',
+                icon: <AssignmentOutlined />
+            },
+            {
+                title: 'Illustrator',
+                desc: 'Tempor dolor ad irure est Lorem magna ea culpa minim anim aliqua.',
+                icon: <WebOutlined />
+            },
+        ],
+
+        skills: [
+            {
+                title: 'FRONT-END', 
+                desc: [
+                "ReactJS",
+                "JavaScript",
+                "Typescript",
+                "Bootstrap",
+                "Material UI",
+
+            ],
+        },
+            {
+                title: 'BACK-END', 
+                desc: [
+                "NodeJS",
+                "NextJS",
+                "Python",
+
+            ],
+        },
+            {
+                title: 'DATABASES', 
+                desc: [
+                "Firebase",
+                "MongoDB",
+
+            ],
+        },
+            {
+                title: 'SOURCE CONTROL', 
+                desc: [
+                "Git",
+                "GitHub",
+            ],
+        },
+        ],
+};
